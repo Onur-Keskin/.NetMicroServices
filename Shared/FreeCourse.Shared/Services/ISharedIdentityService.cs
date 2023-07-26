552041c04c026b353d45f7ae9209a748568d0000
Hüseyin Onur Keskin <h.onur.keskin@hotmail.com>
2023-07-26 13:42:13 +0300

    SharedIdentityService Created
    SharedIdentityService Created

---
+++ Shared/FreeCourse.Shared/Services/ISharedIdentityService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.Services
+{
+    internal interface ISharedIdentityService
+    {
+        public string GetUserId { get;}
+    }
+}
