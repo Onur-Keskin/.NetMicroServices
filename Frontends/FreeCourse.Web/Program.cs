0da237717806403d55d183ffc874b3525ee19f03
Hüseyin Onur Keskin <h.onur.keskin@hotmail.com>
2023-08-26 11:02:56 +0300

    TokenDelegateHandler
    TokenDelegateHandler created

--- Frontends/FreeCourse.Web/Program.cs
+++ Frontends/FreeCourse.Web/Program.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Web.Handler;
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services;
 using FreeCourse.Web.Services.Interfaces;
@@ -19,7 +20,7 @@ var serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").
 builder.Services.AddHttpClient<IUserService, UserService>(opt =>
 {
     opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
-});
+}).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).
     AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opts =>
